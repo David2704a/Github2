a5cbccdfcee33603c26f72b18d6b8eddfc38e388
Johan Andres ch <jachate7@misena.edu.co>
2022-10-10 16:33:09 -0500

    juego adivinar numero e imagen

---
+++ johan/Johan.py
@@ -0,0 +1,18 @@
+import random
+print("HOLA")
+for i in range(3):
+    print("soy Johan Andres Chate ")
+print("----Juego adivinar numero -------")
+al=random.randint(1,100)
+cont=0
+while True:
+    n=int(input("digitar numero\n"))
+    cont +=1
+    if n>al:
+        print("digitar un numero menor\n")
+    elif n<al:
+        print("digitar uno mayor\n")
+    else:
+        print(f"has adivinado el numero era:\t {al}\n") 
+        break
+print(f"los intentos fueron:\n{cont}")
